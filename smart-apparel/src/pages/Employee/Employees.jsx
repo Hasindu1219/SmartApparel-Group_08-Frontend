a75f60de2b373134eb65051f4b1c58b36d285f89
yash-liyanage <127401521+madhushansliyanage@users.noreply.github.com>
2024-04-26 01:52:43 +0530

    Update of employee function

--- smart-apparel/src/pages/Employee/Employees.jsx
+++ smart-apparel/src/pages/Employee/Employees.jsx
@@ -15,13 +15,34 @@ import Stack from "@mui/material/Stack";
 import CustomizedTables from "../../components/EmployeeComponent/EmployeeTable2";
 import ColorButtons from "../../components/EmployeeComponent/Button";
 
-import EmployeeTable from "../../components/EmployeeComponent/EmployeeTable";
+import EmployeeTable2 from "../../components/EmployeeComponent/EmployeeTable2";
+import EmployeeTable3 from "../../components/EmployeeComponent/EmployeeTable3";
+import EmployeeTable4 from "../../components/EmployeeComponent/EmployeeTable4";
 
 const gradientStyle = {
   background: "linear-gradient(to right, #2980B9, #6DD5FA)", // Gradient colors
 };
 
+
+
 export default function Employees() {
+  
+  const EmployeesList =[{
+    empId: "emp001",
+    name: "Michael Johnson",
+    address: "789 Oak Avenue",
+    nic: "543216789Z",
+    position: "sales associate",
+    email: "michael@example.com",
+    password: "securepassword",
+    phoneNumber: "+9551234567",
+    dateOfBirth: "1988-09-20",
+    accountNumber: "1357924680",
+    holderName: "Michael Johnson",
+    branchName: "West Branch",
+    bankName: "Test Bank"
+  }];
+
   return (
     <>
       <Navbar />
@@ -70,24 +91,26 @@ export default function Employees() {
 
           <Box height={30} />
 
-          <Grid container spacing={2}>
+        
+          <EmployeeTable4 rows={EmployeesList}/>
+          
+
+          {/* <Grid container spacing={2}>
             <Grid item xs={8}>
-              <Card
-                sx={{
-                  minHeight: 60 + "vh",
-                  minWidth: 1090,
-                  background: 'linear-gradient(to top left, #ccffff 0%, #ffffff 100%)',
-                }}
-              >
+              <Card sx={{minHeight: 60 + "vh",minWidth: 1090,background: 'linear-gradient(to top left, #ccffff 0%, #ffffff 100%)'}}>
+                
                 <CardContent style={{ textAlign: "center" }}>
-                  {/* <CustomizedTables /> */}
+                  <CustomizedTables />
                   <EmployeeTable/>
+                  <EmployeeTable2/>
+                  <EmployeeTable3/>
                   <ColorButtons />
 
                 </CardContent>
               </Card>
             </Grid>
-          </Grid>
+          </Grid> */}
+          
         </Box>
       </Box>
     </>
--- smart-apparel/src/pages/Salary/SalaryHomePage.js
+++ smart-apparel/src/pages/Salary/SalaryHomePage.js
@@ -1,11 +1,22 @@
+import React from "react";
+import { Box } from "@mui/material";
 import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar";
 
 export default function SalaryHomePage(){
 
 
     return (
-        <Navbar>
-        
-        </Navbar>
+        <>
+            <Navbar/>
+            <Box height={60} />
+            <Box sx={{ display: "flex" }}>
+                <Sidebar/>
+                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                    <h1>Salary</h1>
+                    
+                </Box>
+            </Box>
+        </>
     );
 }
