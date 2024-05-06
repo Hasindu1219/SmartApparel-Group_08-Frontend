b222c16066913fdf039e8ef92202e3e6f8fe978d
SubodhaVirajini <subodha_2020718@fot.ruh.ac.lk>
2024-05-06 20:29:18 +0530

    Home Page Recreation

--- smart-apparel/src/Assets/WebAssets/WAssets/new_collections.js
+++ smart-apparel/src/Assets/WebAssets/WAssets/new_collections.js
@@ -1,10 +1,15 @@
 import p1_img from "./product_12.png";
 import p4_img from "./product_8.png";
-import p6_img from "./product_2.png";
+import B1 from "./B1.jpg";
+import B2 from "./B2.jpg";
+import F1 from "./F1.jpg";
+import F2 from "./F2.jpg";
+import T1 from "./T1.jpg";
+import T2 from "./T2.jpg";
 
 let new_collections = [
   {
-    id: 12,
+    id: 1,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     image: p1_img,
@@ -12,7 +17,7 @@ let new_collections = [
     old_price: 80.5,
   },
   {
-    id: 8,
+    id: 2,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
     image: p4_img,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
@@ -20,49 +25,49 @@ let new_collections = [
     old_price: 150.0,
   },
   {
-    id: 2,
+    id: 3,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
+    image: B1.jpg,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 2,
+    id: 4,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
+    image: B2.jpg,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 2,
+    id: 5,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
+    image: F1.jpg,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 2,
+    id: 6,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
+    image: F2.jpg,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 2,
+    id: 7,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
+    image: T1.jpg,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 2,
+    id: 8,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p6_img,
+    image: T2.jpg,
     description:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
     new_price: 85.0,
     old_price: 120.5,
