c362ccf082764877999758919a74b13a7c10d82a
Hasindu1219 <maneth_2020735@fot.ruh.ac.lk>
2024-05-18 23:00:54 +0530

    edited report structure

--- smart-apparel/src/components/Report/ReportStructure.jsx
+++ smart-apparel/src/components/Report/ReportStructure.jsx
@@ -13,19 +13,7 @@ export default function ReportCard(props) {
 
   return (
     <MDBCard>
-      <MDBRipple
-        rippleColor="light"
-        rippleTag="div"
-        className="bg-image hover-overlay"
-      >
-        <MDBCardImage src={props.image} fluid alt="..." />
-        <a>
-          <div
-            className="mask"
-            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-          ></div>
-        </a>
-      </MDBRipple>
+      
       <MDBCardBody>
         <MDBCardTitle style={{ fontWeight: 'bold', textAlign: 'center' }}>{props.title}</MDBCardTitle>
         <MDBCardText style={{ textAlign: 'center' }}>{props.content}</MDBCardText>
